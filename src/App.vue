524342b17f0d15f680df74cda481e053c9dbc83f
kykyxDD <kykyxDD@gmail.com>
2018-02-09 03:50:27 +0300

    create app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <div v-for="todo in oper">
+      <router-link :to="'/operator/' + todo">Go to {{todo}}</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return{
+      oper: ['mtc', 'megafon']
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Operator.vue
@@ -0,0 +1,49 @@
+/*var operator = Vue.extend({ 
+  template: '#operator',
+  watch: {
+    '$route' (to, from) {
+      // обработка изменений параметров пути...
+    }
+  }
+});
+
+export default operator;*/
+
+<template>
+    <div class='cont-enter-number'>
+    	<div class='title'>foo {{$route.params.name}}</div> 
+    	<router-link to="/enterNumber">Go enter number</router-link>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Operator',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      // обработка изменений параметров пути...
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+.cont-enter-number{
+
+  .title{
+  color: red;
+
+  }
+  a{
+  color: blue;
+  }
+}
+
+</style>
